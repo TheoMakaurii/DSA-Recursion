b320e9b299d2e0f49fd0c9d9232f6a4fd6a25173
Theo McCauley <TheoMakaurii@gmail.com>
2020-10-26 12:40:01 -0400

    power calculator and reverse string

--- Recursion.js
+++ Recursion.js
@@ -13,3 +13,27 @@ function test(test){
     }
     
     console.log(countingSheep(3))
+    
+    function powerCalculator(a, n){
+      if(n === 0){
+        return 1
+      }
+      if(n< 0){
+        return 'n value must be more than zero'
+      }
+    
+      return a* powerCalculator(a, n-1)
+    }
+    
+    console.log(powerCalculator(10, 2))
+    
+    function reverseString(string){
+      if(string.length===1){
+        return string
+      }
+      let reverseIndex=1
+      let reverse = string[string.length-reverseIndex]
+      return reverse + reverseString(string.slice(0,-1))
+    
+    }
+    console.log(reverseString('Theo'))
