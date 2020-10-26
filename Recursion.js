b50a455622c6e4fac7b476b52a4cc5aba4c7fab0
Theo McCauley <TheoMakaurii@gmail.com>
2020-10-26 11:48:28 -0400

    counting sheep

--- Recursion.js
+++ Recursion.js
@@ -1,8 +1,15 @@
 function test(test){
 
-  console.log(test.toUpperCase()+'!!!!')
-}   
-
-test('Theo')
+    console.log(test.toUpperCase()+'!!!!')
+    }
+    test('have fun')
     
+    function countingSheep(sheep){
+      if(sheep === 0){
+        return 'All sheep jumped over the fence!'
+      }
     
+      return `${sheep} sheep and one jumped over the fence!   ` + countingSheep(sheep -1);
+    }
+    
+    console.log(countingSheep(3))
